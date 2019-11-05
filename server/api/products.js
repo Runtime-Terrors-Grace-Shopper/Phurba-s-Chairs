746e88322d008acbbf1c4e2ed9dbe80a381cbea3
Sam Miller <sammillerks@gmail.com>
2019-11-05 14:33:59 -0500

    updated express api product routes

--- server/api/products.js
+++ server/api/products.js
@@ -11,8 +11,21 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+router.post('/', async (req, res, next) => {
+  try {
+      if (!req.body) {
+          return res.sendStatus(400)
+      }
+      const newProduct = await Product.create(req.body)
+      res.json(newProduct)
+  } catch (error) {
+      next(error)
+  }
+
+})
+
 router.get('/:id', async (req, res, next) => {
-    const product = await Product.findById(req.params.id) 
+    const product = await Product.findByPk(req.params.id) 
     if (product === null) {
         res.sendStatus(404);
     } else {
@@ -20,4 +33,28 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+
+router.put(':id', async (req, res, next) => {
+  try {
+    const targetProduct = await Product.findByPk(req.params.id)
+    const updatedProduct = await targetProduct.update(req.body)
+    res.status(202).json(updatedProduct)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+      await Product.destroy({
+          where: {
+              id: req.params.id
+          }
+      })
+      res.sendStatus(204)
+  } catch (error) {
+      next(error)
+  }
+})
 module.exports = router
+

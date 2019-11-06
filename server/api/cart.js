7a4054af6ab1d92ad77032c308c8c99eac883b99
Jordan Contract <jordan59@optonline.net>
2019-11-06 11:49:03 -0500

    cart routes

---
+++ server/api/cart.js
@@ -0,0 +1,62 @@
+const router = require('express').Router()
+const {User, Order, Product, OrderProduct} = require('../db/models')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Order.findOne({
+      where: {
+        userId: req.user.id,
+        status: 'Active'
+      },
+      include: [{model: Product}]
+    })
+    res.json(data.products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const cartItem = await OrderProduct.findByPk(req.params.id)
+    if (!cartItem) res.sendStatus(404)
+    else res.json(cartItem)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newItem = await OrderProduct.create(req.body)
+    res.status(201).send(newItem)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const targetItem = await OrderProduct.findByPk(req.params.id)
+    if (targetItem.quantity <= 1) {
+      await targetItem.destroy()
+      res.sendStatus(204)
+    } else {
+      targetItem.quantity--
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const itemToEdit = await OrderProduct.findByPk(req.params.id)
+    const updatedItem = await itemToEdit.update(req.body)
+    res.status(200).send(updatedItem)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
